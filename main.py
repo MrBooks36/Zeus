1e058bf2f753df0f7051c380cf685b7d63e3154b
MrBooks36 <mrbooks36@outlook.com>
2025-06-26 16:19:17 +1000

    no more robot head

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from threading import Thread
-from ohbot import ohbot
 from ai import ai
-from look import look
 from os.path import exists
-import requests
-ohbot.reset()
-ohbot.detach(ohbot.HEADTURN)
-ohbot.detach(ohbot.HEADROLL)
 
-thr = Thread(target=look, daemon=True)
-thr.start()
 
 def start_ai(pram):
    try:
@@ -17,15 +8,6 @@ def start_ai(pram):
    except Exception as e:
       with open('ERROR.txt', 'w') as file: 
         file.write(str(e))
-
-def is_site_up(url):
-    try:
-        response = requests.get(url)
-        # If the response status code is less than 400, the site is considered up.
-        return response.status_code < 400
-    except requests.exceptions.RequestException:
-        # If there is any request exception, the site is considered down.
-        return False
 
 if not exists('vosk'): start_ai(True)
 else: start_ai(False)
